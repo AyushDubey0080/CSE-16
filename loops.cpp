4e3970993b8666ce6ff4a451ea3dc6fb078edc50
Ayush Dubey <ayush3003dubey@gmail.com>
2025-11-13 09:12:29 +0530

    Final

---
+++ loops.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num,remainder,result,n;
+    while (num!=0){
+        num=num/10;
+        n=n+1;
+    }
+    while (num!=0)
+    remainder=result +(remainder^n);
+    num=num/10;
+    if (result==num)
+} 
